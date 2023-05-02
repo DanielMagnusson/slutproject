3824ffc076600a317eaf232981ac71e6baaf5e01
DanielMagnusson <Daniel.Magnusson@skola.taby.se>
2023-05-02 09:46:17 +0200

    test fixed

--- test.js
+++ test.js
@@ -12,9 +12,6 @@ showSlides3(slideIndex3);
 
 // Next/previous controls
 function plusSlides(n, slideshowNumber) {
-    console.log(slideIndex1);
-    console.log(slideIndex2);
-    console.log(n);
   if (slideshowNumber === 1) {
     showSlides1(slideIndex1 += n);
   } else if (slideshowNumber === 2) {
@@ -25,10 +22,8 @@ function plusSlides(n, slideshowNumber) {
 }
 
 function showSlides1(n) {
-    console.log("Funktion startar?");
   var i;
   var slides = document.getElementsByClassName("mySlides1");
-  console.log(slides.length);
   if (n > slides.length) {
     slideIndex1 = 1;
   }
@@ -38,20 +33,12 @@ function showSlides1(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  console.log(slideIndex1 - 1);
-  console.log(slides);
-  console.log(slides.length);
-  console.log(slides[slideIndex1 - 1]);
-  console.log(JSON.stringify(slides));
   slides[slideIndex1 - 1].style.display = "block";
-  //slides.item(slideIndex1-1).style.display = "block";
-  console.log("Funktion färdig?");
 }
 
 function showSlides2(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides2");
-  console.log(n);
   if (n > slides.length) {
     slideIndex2 = 1;
   }
@@ -61,7 +48,6 @@ function showSlides2(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  console.log(slideIndex2);
   slides[slideIndex2 - 1].style.display = "block";
 }
 
@@ -74,9 +60,7 @@ function showSlides3(n) {
   if (n < 1) {
     slideIndex3 = slides.length;
   }
-  console.log(slides)
   for (i = 0; i < slides.length; i++) {
-    console.log(slides[i])
     slides[i].style.display = "none";
   }
   slides[slideIndex3 - 1].style.display = "block";
